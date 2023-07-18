928e98debcccd615ae2a8c75ad7c3646eaf62e4d
Donald Mennerich <don.mennerich@nyu.edu>
2023-07-18 13:56:14 -0400

    adding test for altered headers

--- main_test.go
+++ main_test.go
@@ -6,14 +6,27 @@ import (
 )
 
 func TestCompareEAD(t *testing.T) {
-	t.Run("testing compare changed archdescs", func(t *testing.T) {
-		origPath := "test/mss_360_Orig.xml"
-		origBytes, err := GetArchDescBytes(origPath)
+	origPath := "test/mss_360_Orig.xml"
+	origBytes, err := GetArchDescBytes(origPath)
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Run("testing compare changed headers", func(t *testing.T) {
+		alteredPath := "test/mss_360_AlteredHeader.xml"
+		alteredBytes, err := GetArchDescBytes(alteredPath)
 		if err != nil {
 			t.Error(err)
 		}
+		match := (string(origBytes) == string(alteredBytes))
+		if match != true {
+			t.Error(fmt.Errorf("ArchDescs are not the same"))
+		}
+	})
 
-		alteredPath := "test/mss_360_Altered.xml"
+	t.Run("testing compare changed archdescs", func(t *testing.T) {
+
+		alteredPath := "test/mss_360_AlteredArchDesc.xml"
 		alteredBytes, err := GetArchDescBytes(alteredPath)
 		if err != nil {
 			t.Error(err)
